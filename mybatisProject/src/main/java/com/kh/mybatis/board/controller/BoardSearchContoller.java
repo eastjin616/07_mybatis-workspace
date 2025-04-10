00ac6345a7a7e1e24fd1687c49cbcebccdb5c918
seo <djseo9812@gmail.com>
2025-04-10 15:47:41 +0900

    04/10

---
+++ mybatisProject/src/main/java/com/kh/mybatis/board/controller/BoardSearchContoller.java
@@ -0,0 +1,60 @@
+package com.kh.mybatis.board.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.mybatis.board.model.service.BoardSeriveImpl;
+import com.kh.mybatis.common.model.vo.PageInfo;
+import com.kh.mybatis.common.template.Pagination;
+
+/**
+ * Servlet implementation class BoardSearchContoller
+ */
+@WebServlet("/search.bo")
+public class BoardSearchContoller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardSearchContoller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String condition = request.getParameter("condition");//"writer"|"title"|"content"
+		String keyword = request.getParameter("keyword"); //사용자가 입력한 키워드 값
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+
+		BoardSeriveImpl bService = new BoardSeriveImpl();
+		int searchCount = bService.selectSearchCount(map); // 현재 검색 결과에 맞는 게시글 총 갯수
+		int currentPage = Integer.parseInt(request.getParameter("cpage"));
+		
+		PageInfo pi =  Pagination.getPageInfo(searchCount, currentPage, 10, 5);
+		
+		System.out.println(pi);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- mybatisProject/webapp/WEB-INF/views/common/menubar.jsp
+++ mybatisProject/webapp/WEB-INF/views/common/menubar.jsp
@@ -34,11 +34,11 @@
 
     .outer{
         width: 900px;
+        height: 600px;
         background-color: black;
         color: white;
         margin: auto;
         margin-top: 50px;
-        height: 550px;
     }
 </style>
 </head>
@@ -98,7 +98,7 @@
     <div class="nav-area" align="center">
         <div class="menu">HOME</div>
         <div class="menu">공지사항</div>
-        <div class="menu">게시판</div>
+        <div class="menu" onclick="location.href='list.bo?cpage=1'">게시판</div>
         <div class="menu">ETC</div>
     </div>
 
